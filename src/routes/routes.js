c7911d23fd05bb02545542f55a281f85fa5d2844
vienle15 <tanvien93lt@gmail.com>
2023-11-01 09:27:01 +0700

    up

--- src/routes/routes.js
+++
@@ -1,7 +0,0 @@
-export const routes = [
-  {
-    path: "/",
-    title: "home",
-    content: "",
-  },
-];
--- src/server.js
+++ src/server.js
@@ -1,53 +1,34 @@
 import express from "express";
-import bodyParser from "body-parser";
-
-// config evn
 import "dotenv/config";
-import { route } from "./routes/index.js";
+import fs from "fs";
 
-//thay thế cho server trước đây
 const app = express();
 
-// dùng middleware bodyParser để lấy dữ liệu từ body http request
-app.use(bodyParser.urlencoded({ extended: false }));
+// lựa chọn template sử dụng
+app.set("view engine", "ejs"); //--> mặc định folder views nằm cùng cấp source
+app.set("views", "src/views"); // --> set lại đường dẫn thực tế của folder views
 
-route(app);
+// routing
 
-// Phương thức get --> yêu cầu dữ liệu
-// app.get("/products", (req, res) => {
-//   //res.write() --> res.end()
+app.get("/", (req, res) => {
+  res.render("index", {
+    content: "Hello world",
+    students: ["Quyết", "Hạnh", "Thái"],
+  });
+});
 
-//   //   lấy từ database để trả về
-//   res.send([
-//     { id: 1, name: "products 1" },
-//     { id: 2, name: "products 2" },
-//   ]);
-// });
-// app.get("/products/:id", (req, res) => {
-//   // Để lấy params --> req.params
-//   console.log("Lấy id: ", req.params);
-// });
+app.get("/users", (req, res) => {
+  const dataJSON = fs.readFileSync("./src/model/users.json", "utf8");
 
-// app.get("/account/:course/:lesson", (req, res) => {
-//   // Để lấy params --> req.params
-//   console.log("Lấy id: ", req.params);
-// });
+  const dataArray = JSON.parse(dataJSON);
 
-// app.post("/login", (req, res) => {
-//   //res.write() --> res.end()
-//   console.log("Kiểm tra req", req.body);
-//   res.send("Hello data");
-// });
+  res.render("user", {
+    data: dataArray,
+  });
+});
 
-// dùng với characters *
-// app.get("/*", (req, res) => {
-//   //res.write() --> res.end()
-//   res.send("Page not found");
-// });
+const port = process.env.PORT || 3000;
 
-// lấy PORT từ dotenv
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Connecting to http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`connecting to http://localhost:${port}`);
 });
